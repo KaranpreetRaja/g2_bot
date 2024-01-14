bfac3c1f6629f846257f11c7ff1ac23c6f907adc
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-01-13 22:32:14 -0500

    Creates DTO for company

---
+++ api/DTO/companyDTO.py
@@ -0,0 +1,8 @@
+class CompanyDTO:
+    def __init__(self, name, ratings, overview, prices, features):
+        self.name = name
+        self.ratings = ratings
+        self.overview = overview
+        self.prices = prices
+        self.features = features
+        
