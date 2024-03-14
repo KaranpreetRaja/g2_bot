7dd6768fd2c1d5d7c02b21b711aae422e149f2ee
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-03-13 21:31:52 -0400

    Adds url to company DTO

--- api/DTO/companyDTO.py
+++ api/DTO/companyDTO.py
@@ -1,6 +1,7 @@
 class CompanyDTO:
-    def __init__(self, name, ratings, overview, prices, features):
+    def __init__(self, name, url, ratings, overview, prices, features):
         self.name = name
+        self.url = url
         self.ratings = ratings
         self.overview = overview
         self.prices = prices
